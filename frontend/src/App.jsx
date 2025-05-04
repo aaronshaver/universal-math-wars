52b71cc023f9204b4d83e7d43e994ad1533d1a26
Aaron Shaver <aaronshaverpdx@gmail.com>
2025-05-04 15:29:44 -0700

    add React and Bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello World</h1>
+      <button className="btn btn-primary">Start</button>
+    </div>
+  );
+}
+
+export default App;
