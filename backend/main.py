09774155647ae733ee8cf7f48af2966aa57db879
Aaron Shaver <aaronshaverpdx@gmail.com>
2025-05-05 10:52:30 -0700

    add Fast API and get_uuid endpoint

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from .registration import generate_uuid_string
+
+app = FastAPI(title="Universal Math Wars API")
+
+
+@app.get("/api/v1/generate-uuid")
+async def get_uuid():
+    generated_uuid = generate_uuid_string()
+    return {"uuid": generated_uuid} 
